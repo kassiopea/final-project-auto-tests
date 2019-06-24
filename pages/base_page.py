4a7a37563258d7e80205e38acb20d340297c8b4d
Anastasia Salikova <randomnanastya@gmail.com>
2019-06-24 18:29:56 +0300

    add def element is present with try exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,20 @@
 # Для начала сделаем базовую страницу, от которой будут унаследованы все остальные классы.
 # В ней мы опишем вспомогательные методы для работы с драйвером.
+from selenium.common.exceptions import NoSuchAttributeException
 
 
 class BasePage(object):
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except:
+            return False
+        return True
