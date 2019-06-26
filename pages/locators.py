690d263c9d82d04c09dd2f7e409e22756c2bb5e1
Anastasia Salikova <randomnanastya@gmail.com>
2019-06-26 19:18:26 +0300

    final edits add pytest mark

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-# class MainPageLocators(object):
-#     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class ProductPageLocators(object):
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
     MESSAGE_AFTER_ADD_ITEM = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
@@ -17,11 +13,16 @@ class ProductPageLocators(object):
 class BasePageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#register_form #id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#register_form #id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#register_form #id_registration-password2")
+    REGISTER_SUBMIT = (By.NAME, "registration_submit")
 
 
 class CardPageLocators(object):
--- test_main_page.py
+++ test_main_page.py
@@ -2,35 +2,38 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.card_page import CardPage
 
+import pytest
 
-# def go_to_login_page(browser):
-#     link = browser.find_element_by_css_selector("#login_link")
-#     link.click()
-#
-#
-# def test_guest_can_go_to_login_page(browser):
-#     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-#     link = "http://selenium1py.pythonanywhere.com"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#
-#
-# def test_guest_should_see_login_link(browser):
-#     # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-#     link = "http://selenium1py.pythonanywhere.com"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-#
-#
-# def test_guest_can_go_to_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    def test_guest_can_go_to_login_page(self, browser):
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
