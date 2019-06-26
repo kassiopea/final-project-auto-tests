6d98c923170fc6b10410d2525efd5550cf3b01a4
Anastasia Salikova <randomnanastya@gmail.com>
2019-06-26 19:18:26 +0300

    final edits

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,17 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
+    def register_new_user(self, email, password):
+        reg_email = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        reg_email.send_keys(email)
+        reg_pass = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD)
+        reg_pass.send_keys(password)
+        confirm_pass = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        confirm_pass.send_keys(password)
+        submit = self.browser.find_element(*LoginPageLocators.REGISTER_SUBMIT)
+        submit.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
